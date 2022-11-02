f946344ffb6fc58811bc69deb264e7e5cfac6e1c
Phill Legnitto <palegnitto@gmail.com>
2022-11-01 22:27:12 -0700

    restructured props

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import About from "../pages/About/About";
+import Blog from "../pages/Blog/Blog";
+import Home from "../pages/Home/Home";
+import Projects from "../pages/Projects/Projects";
+import Resume from "../pages/Resume/Resume";
+
+const pages = {
+  About,
+  Blog,
+  Home,
+  Projects,
+  Resume,
+}
+
+export default pages;
